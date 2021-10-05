4e5425b9c425ca097a0d2f6b0aa54fb4c29dcb5b
unknown <guilhermeguimti@gmail.com>
2021-10-05 00:24:28 -0300

    [Feature Guilherme - Create start]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 
@@ -12,7 +13,9 @@ const routes: Routes = [
 
 
 {path:'entrar', component: EntrarComponent},
-{path: 'cadastrar', component: CadastrarComponent}
+{path: 'cadastrar', component: CadastrarComponent},
+{path: 'inicio', component: InicioComponent}
+
 
 
 
